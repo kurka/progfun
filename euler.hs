797ab8cdfd41b50bc89dc89de4832095f3633ce1
kurka <david.kurka@gmail.com>
2017-04-24 23:29:18 +0100

    problem 5

--- euler.hs
+++ euler.hs
@@ -1,3 +1,7 @@
 
 p004 = maximum [(a*b, a, b) | a <- [999,998..100], b <- [999,998..a], palindrome (a*b)]
   where palindrome x = (show x) == (reverse $ show x)
+
+
+-- p005 = head [x | x <- [2520..], all ((==0) . (mod x)) [2..20]]
+p005 = 16*9*5*7*11*13*17*19
