7adfa4f976b0831a68798b4107f962605f1359ca
David Kurka <david.kurka@gmail.com>
2021-12-15 21:07:26 -0300

    learning the hard way that range copies by value and not by reference

--- advent2021/day4.go
+++ advent2021/day4.go
@@ -16,32 +16,28 @@ type Coordinate struct {
 }
 
 type CardData struct {
-	card Card
-	marked map[int]Coordinate
-	unmarked map[int]Coordinate
-	rowsCounter [CardSize]int
+	card           Card
+	marked         map[int]Coordinate
+	unmarked       map[int]Coordinate
+	rowsCounter    [CardSize]int
 	columnsCounter [CardSize]int
-
 }
 
-func readinput() (draws []int, cards []CardData) {
+func readinput4() (draws []int, cards []CardData) {
 	var draws_string string
 	// first read the list of draws
 	fmt.Scan(&draws_string)
 
-	fmt.Println("a", draws_string)
 	for _, d := range strings.Split(draws_string, ",") {
 		draw, _ := strconv.Atoi(d)
 		draws = append(draws, draw)
 	}
-	fmt.Println("draws", draws)
 
 	// then read the cards
-	// var space string // couldn't make it work without reading this variable
 CardLoop:
 	for {
 		var card Card
-	    unmarked := make(map[int]Coordinate, CardSize*CardSize)
+		unmarked := make(map[int]Coordinate, CardSize*CardSize)
 		var card_value int
 		for i := 0; i < CardSize; i++ {
 			for j := 0; j < CardSize; j++ {
@@ -60,39 +56,34 @@ CardLoop:
 	return
 }
 
-func solve1(draws []int, cards []CardData) int {
+func solve4a(draws []int, cards *[]CardData) (result int) {
 
-	var result int
 	for _, draw := range draws {
-		fmt.Println("Drew", draw)
-		for _, card := range cards {
-			fmt.Println("checking:", card)
+		for card_i := range *cards {
+			card := &(*cards)[card_i]
 			if coords, ok := card.unmarked[draw]; ok {
 				delete(card.unmarked, draw)
 				card.marked[draw] = coords
 				card.rowsCounter[coords.row] += 1
 				card.columnsCounter[coords.column] += 1
-				fmt.Println("deleted", draw, card)
 			}
 			for i := range card.rowsCounter {
-				if card.rowsCounter[i] == CardSize || card.columnsCounter[i] == CardSize{
-					unmarked_sum := 0;
+				if card.rowsCounter[i] == CardSize || card.columnsCounter[i] == CardSize {
+					unmarked_sum := 0
 					for unmarked := range card.unmarked {
 						unmarked_sum += unmarked
 					}
 					result = unmarked_sum * draw
+					return result
 				}
 			}
 		}
 	}
 	return result
 
-
 }
 
 func main() {
-	draws, cards := readinput()
-	fmt.Println(draws)
-	fmt.Println(cards)
-	fmt.Println(solve1(draws, cards))
+	draws, cards := readinput4()
+	fmt.Println(solve4a(draws, &cards))
 }
