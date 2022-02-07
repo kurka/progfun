42399be41cb444f1843c1412ddb8750189c64003
David Kurka <david.kurka@gmail.com>
2022-02-06 23:09:10 -0300

    generalizing the code to support any depth of rooms
    but stlil too slow to answer part 2

--- advent2021/day23.go
+++ advent2021/day23.go
@@ -18,10 +18,17 @@ type State struct {
 }
 
 func (s State) String() string {
-	hallway := fmt.Sprintf("#%c%c.%c.%c.%c.%c%c#", s.hallway[0], s.hallway[1], s.hallway[2], s.hallway[3], s.hallway[4], s.hallway[5], s.hallway[6])
-	rooms1 := fmt.Sprintf("###%c#%c#%c#%c###", s.rooms[0].fst, s.rooms[1].fst, s.rooms[2].fst, s.rooms[3].fst)
-	rooms2 := fmt.Sprintf("###%c#%c#%c#%c###", s.rooms[0].snd, s.rooms[1].snd, s.rooms[2].snd, s.rooms[3].snd)
-	return fmt.Sprintf("%s\n%s\n%s\n", hallway, rooms1, rooms2)
+	// add hallway
+	sString := fmt.Sprintf("#%c%c.%c.%c.%c.%c%c#\n", s.hallway[0], s.hallway[1], s.hallway[2], s.hallway[3], s.hallway[4], s.hallway[5], s.hallway[6])
+	// add rooms
+	for d := range s.rooms[0].slots {
+		sString += "###"
+		for i := range s.rooms {
+			sString += fmt.Sprintf("%c#", s.rooms[i].slots[d])
+		}
+		sString += "##\n"
+	}
+	return sString
 }
 
 func (m Move) String() string {
@@ -29,7 +36,7 @@ func (m Move) String() string {
 }
 
 type Room struct {
-	fst, snd rune
+	slots []rune
 }
 
 type Move struct {
@@ -37,8 +44,8 @@ type Move struct {
 }
 
 type Pos struct {
-	ltype string
-	pos   int
+	ltype      rune
+	pos, depth int
 }
 
 var hallIdx2Pos = map[int]int{
@@ -68,19 +75,22 @@ func intAbs(x int) int {
 
 func readInput23() (initState State) {
 
+	for i := 0; i < numRooms; i++ {
+		initState.rooms[i].slots = make([]rune, 2)
+	}
 	fmt.Println(initState)
 	fmt.Scanf("#############\n")
 	fmt.Scanf("#...........#\n")
 	fmt.Scanf("###%c#%c#%c#%c###\n",
-		&initState.rooms[0].fst,
-		&initState.rooms[1].fst,
-		&initState.rooms[2].fst,
-		&initState.rooms[3].fst)
+		&initState.rooms[0].slots[0],
+		&initState.rooms[1].slots[0],
+		&initState.rooms[2].slots[0],
+		&initState.rooms[3].slots[0])
 	fmt.Scanf("  #%c#%c#%c#%c#\n",
-		&initState.rooms[0].snd,
-		&initState.rooms[1].snd,
-		&initState.rooms[2].snd,
-		&initState.rooms[3].snd)
+		&initState.rooms[0].slots[1],
+		&initState.rooms[1].slots[1],
+		&initState.rooms[2].slots[1],
+		&initState.rooms[3].slots[1])
 	fmt.Scanf("  #########\n")
 
 	for i := range initState.hallway {
@@ -106,8 +116,10 @@ func minimum(list []int) (int, int) {
 
 func isFinalState(state State) bool {
 	for i, room := range state.rooms {
-		if room.fst != rune('A'+i) || room.snd != rune('A'+i) {
-			return false
+		for _, slot := range room.slots {
+			if slot != rune('A'+i) {
+				return false
+			}
 		}
 	}
 	return true
@@ -131,7 +143,15 @@ func getPossibleMoves(state State) (possibleMoves []Move) {
 				}
 			}
 		}
+		return true
+	}
 
+	restSolved := func(room Room, pos int, expectedContent rune) bool {
+		for i := pos + 1; i < len(room.slots); i++ {
+			if room.slots[i] != expectedContent {
+				return false
+			}
+		}
 		return true
 	}
 
@@ -139,20 +159,21 @@ func getPossibleMoves(state State) (possibleMoves []Move) {
 		if hallContent != empty {
 			desiredRoomIdx := int(hallContent - 'A')
 			desiredRoom := state.rooms[desiredRoomIdx]
-			if clearPath(hallPos, desiredRoomIdx) && desiredRoom.snd == empty { // try to move to bottom of room first
-				possibleMoves = append(possibleMoves, Move{Pos{"h", hallPos}, Pos{"r2", desiredRoomIdx}})
-			} else if clearPath(hallPos, desiredRoomIdx) && desiredRoom.snd == hallContent && desiredRoom.fst == empty { // or to first part
-				possibleMoves = append(possibleMoves, Move{Pos{"h", hallPos}, Pos{"r1", desiredRoomIdx}})
+			for depth := len(desiredRoom.slots) - 1; depth >= 0; depth-- { // try to move to bottom of room first
+				if desiredRoom.slots[depth] == empty && clearPath(hallPos, desiredRoomIdx) && restSolved(desiredRoom, depth, hallContent) {
+					possibleMoves = append(possibleMoves, Move{Pos{'h', hallPos, -1}, Pos{'r', desiredRoomIdx, depth}})
+					break
+				}
 			}
 		}
 	}
 
 	// evaluate room -> hallway moves
-	findHallwayMoves := func(roomPos int, roomType string) {
+	findHallwayMoves := func(roomPos, roomDepth int) {
 		// find empty hallways positions on the left of room
 		for i := roomPos + 1; i >= 0; i-- {
 			if state.hallway[i] == empty {
-				possibleMoves = append(possibleMoves, Move{Pos{roomType, roomPos}, Pos{"h", i}})
+				possibleMoves = append(possibleMoves, Move{Pos{'r', roomPos, roomDepth}, Pos{'h', i, -1}})
 			} else {
 				// stop looking for spaces as soon as the hallway is busy
 				break
@@ -161,7 +182,7 @@ func getPossibleMoves(state State) (possibleMoves []Move) {
 		// find empty hallways positions on the right of room
 		for i := roomPos + 2; i < numRooms+3; i++ {
 			if state.hallway[i] == empty {
-				possibleMoves = append(possibleMoves, Move{Pos{roomType, roomPos}, Pos{"h", i}})
+				possibleMoves = append(possibleMoves, Move{Pos{'r', roomPos, roomDepth}, Pos{'h', i, -1}})
 			} else {
 				// stop looking for spaces as soon as the hallway is busy
 				break
@@ -169,11 +190,29 @@ func getPossibleMoves(state State) (possibleMoves []Move) {
 		}
 	}
 
+	prevEmpty := func(room Room, depth int) bool {
+		for i := depth - 1; i >= 0; i-- {
+			if room.slots[i] != empty {
+				return false
+			}
+		}
+		return true
+	}
+
+	roomNotReady := func(room Room, startDepth int, expectedValue rune) bool {
+		for i := startDepth; i < len(room.slots); i++ {
+			if room.slots[i] != expectedValue {
+				return true
+			}
+		}
+		return false
+	}
+
 	for roomPos, room := range state.rooms {
-		if room.fst != empty && (room.fst != rune('A'+roomPos) || room.snd != rune('A'+roomPos)) {
-			findHallwayMoves(roomPos, "r1")
-		} else if room.fst == empty && room.snd != empty && room.snd != rune('A'+roomPos) {
-			findHallwayMoves(roomPos, "r2")
+		for depth, roomValue := range room.slots {
+			if roomValue != empty && prevEmpty(room, depth) && roomNotReady(room, depth, rune('A'+roomPos)) {
+				findHallwayMoves(roomPos, depth)
+			}
 		}
 	}
 
@@ -183,33 +222,21 @@ func getPossibleMoves(state State) (possibleMoves []Move) {
 func makeMove(state *State, move Move) (cost int) {
 	// change state and compute cost
 	switch move.from.ltype {
-	case "h": // hallway -> room move
-		var vertDist int
-		switch move.to.ltype {
-		case "r1":
-			(*state).rooms[move.to.pos].fst = (*state).hallway[move.from.pos]
-			vertDist = 1
-		case "r2":
-			(*state).rooms[move.to.pos].snd = (*state).hallway[move.from.pos]
-			vertDist = 2
-		}
+	case 'h': // hallway -> room move
+		(*state).rooms[move.to.pos].slots[move.to.depth] = (*state).hallway[move.from.pos]
 		(*state).hallway[move.from.pos] = empty
+		vertDist := move.to.depth + 1
 		costPerMove := intPow(10, move.to.pos)
 		horizDist := intAbs((2 + move.to.pos*2) - hallIdx2Pos[move.from.pos])
 		cost = costPerMove * (horizDist + vertDist)
 
-	case "r1": // room.fst -> hallway move
-		(*state).hallway[move.to.pos] = (*state).rooms[move.from.pos].fst
-		(*state).rooms[move.from.pos].fst = empty
+	case 'r': // room -> hallway move
+		(*state).hallway[move.to.pos] = (*state).rooms[move.from.pos].slots[move.from.depth]
+		(*state).rooms[move.from.pos].slots[move.from.depth] = empty
 		costPerMove := intPow(10, int((*state).hallway[move.to.pos]-'A'))
+		vertDist := move.from.depth + 1
 		horizDist := intAbs((2 + move.from.pos*2) - hallIdx2Pos[move.to.pos])
-		cost = costPerMove * (horizDist + 1)
-	case "r2": // room.snd -> hallway move
-		(*state).hallway[move.to.pos] = (*state).rooms[move.from.pos].snd
-		(*state).rooms[move.from.pos].snd = empty
-		costPerMove := intPow(10, int((*state).hallway[move.to.pos]-'A'))
-		horizDist := intAbs((2 + move.from.pos*2) - hallIdx2Pos[move.to.pos])
-		cost = costPerMove * (horizDist + 2)
+		cost = costPerMove * (horizDist + vertDist)
 	}
 
 	return cost
@@ -219,10 +246,6 @@ func searchAllPaths(state State, costSoFar int, moveChain []Move) int {
 	// fmt.Println(state)
 	if isFinalState(state) {
 		// fmt.Println("final with cost", costSoFar)
-		// fmt.Println(state)
-		if costSoFar == 8519 {
-			fmt.Println(moveChain)
-		}
 		// fmt.Println(moveChain)
 		return costSoFar
 	}
@@ -260,4 +283,23 @@ func main() {
 	initState := readInput23()
 	fmt.Println(initState)
 	solve23(initState)
+	var extendedState State
+	for i := range initState.hallway {
+		extendedState.hallway[i] = initState.hallway[i]
+	}
+	// #D#C#B#A#
+	// #D#B#A#C#
+	var extraSlots [2][4]rune
+	extraSlots[0] = [4]rune{'D', 'C', 'B', 'A'}
+	extraSlots[1] = [4]rune{'D', 'B', 'A', 'C'}
+
+	for i := range initState.rooms {
+		extendedState.rooms[i].slots = make([]rune, 4)
+		extendedState.rooms[i].slots[0] = initState.rooms[i].slots[0]
+		extendedState.rooms[i].slots[1] = extraSlots[0][i]
+		extendedState.rooms[i].slots[2] = extraSlots[1][i]
+		extendedState.rooms[i].slots[3] = initState.rooms[i].slots[1]
+	}
+	fmt.Println(extendedState)
+	solve23(extendedState)
 }
